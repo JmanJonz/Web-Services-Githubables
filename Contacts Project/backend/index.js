7dc7383f17ffd8d10d70ebd501e9a3d1924e04cd
Jammonjones <wjssaan@gmail.com>
2023-09-14 16:35:39 -0600

    dfdf

--- Contacts Project/backend/index.js
+++ Contacts Project/backend/index.js
@@ -27,6 +27,9 @@
     });
 
 // handle requests through imported routers
+    server.get("/", (req, res)=>{
+        res.send("Incomplete Path Add On To It You Dummy");
+    })
     import contactsRouter from "./routers/contactsRouter.js";
     server.use("/contacts", contactsRouter);
 
