5359ff5c4e7d16297fcdb31d2a732cb1abfdbe4f
Jammonjones <wjssaan@gmail.com>
2023-09-20 15:24:48 -0600

    dfdf

---
+++ Contacts Project/backend/utilities/swagger.js
@@ -0,0 +1,12 @@
+// imports
+    import swaggerJSDoc from "swagger-jsdoc";
+    import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Contacts Project",
+            version: "100000"
+        }
+    }
+}
